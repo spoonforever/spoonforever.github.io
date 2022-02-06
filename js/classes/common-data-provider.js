fb6b5790f0d1e8dca37c3e7b91f0d01a66cc8009
yulia <spoonyla@gmail.com>
2022-02-06 16:50:54 +0200

    Change to new url for get items source

--- js/classes/common-data-provider.js
+++ js/classes/common-data-provider.js
@@ -3,8 +3,8 @@
 class CommonDataProvider extends DataProvider {
     constructor() {
         super({
-            getDataUrl: 'https://script.google.com/macros/s/AKfycbwzk8PiH-3QJ3qDyJ8Fwc4-ek0JszTeWJupbX7bQ8NZTPijnKvQUBMLh3OYGWDTWUIW/exec',
-            getIsShouldUpdateUrlPart: 'https://script.google.com/macros/s/AKfycbwzk8PiH-3QJ3qDyJ8Fwc4-ek0JszTeWJupbX7bQ8NZTPijnKvQUBMLh3OYGWDTWUIW/exec?lastUpdated=',
+            getDataUrl: 'https://script.google.com/macros/s/AKfycbx8lYeWGS_7ediKgE1d6aPvDZNupFgE2a8otUVuFy0jXiapZg5lwytb_itLfDQHNodg/exec',
+            getIsShouldUpdateUrlPart: 'https://script.google.com/macros/s/AKfycbx8lYeWGS_7ediKgE1d6aPvDZNupFgE2a8otUVuFy0jXiapZg5lwytb_itLfDQHNodg/exec?lastUpdated=',
             dataKey: 'items-data-spoon-shop',
             lastUpdatedKey: 'last-update-items-data-spoon-shop',
             method: 'GET'
